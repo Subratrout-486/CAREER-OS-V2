296cca452c37d18edbf5a3b8fdd513dab7de1395
Subratrout-486 <subratrout486@gmail.com>
2026-08-21 20:01:18 +0000

    feat: expose Job Scout domain models

---
+++ src/career_os/models/__init__.py
@@ -0,0 +1,3 @@
+from career_os.models.job import JobEvidence, JobRecord, JobStatus, canonical_job_key, canonicalize_url
+
+__all__ = ["JobEvidence", "JobRecord", "JobStatus", "canonical_job_key", "canonicalize_url"]
