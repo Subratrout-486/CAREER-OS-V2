c9f19381c484aefbe20e07c93cd1e76eae553eb9
Subratrout-486 <subratrout486@gmail.com>
2026-08-22 07:55:15 +0000

    feat(rendering): add PDF rendering package

---
+++ src/career_os/rendering/__init__.py
@@ -0,0 +1,4 @@
+from career_os.rendering.pdf import PDFRenderer, PDFValidationError, validate_pdf
+from career_os.rendering.resume import ResumeRenderer
+
+__all__ = ["PDFRenderer", "PDFValidationError", "ResumeRenderer", "validate_pdf"]
