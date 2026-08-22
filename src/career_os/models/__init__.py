c978e4b753871615e4d22ecdd14314c795a0b4ff
Subratrout-486 <subratrout486@gmail.com>
2026-08-22 13:52:28 +0000

    feat: export evidence models

--- src/career_os/models/__init__.py
+++ src/career_os/models/__init__.py
@@ -1,4 +1,5 @@
 from career_os.models.application import ApplicationEvent, ApplicationRecord, ApplicationStatus
+from career_os.models.evidence import EvidenceClaim, EvidenceKind, EvidenceLedger, EvidenceSource, SupportStatus
 from career_os.models.interview import AnswerEvaluation, AnswerScore, InterviewQuestion, InterviewQuestionType, InterviewSession
 from career_os.models.job import JobEvidence, JobRecord, JobStatus, canonical_job_key, canonicalize_url
 from career_os.models.learning import LearningObjective, LearningPlan, LearningPriority, LearningResource, PracticeTask, ReadinessCheck
@@ -9,6 +10,10 @@ __all__ = [
     "ApplicationEvent",
     "ApplicationRecord",
     "ApplicationStatus",
+    "EvidenceClaim",
+    "EvidenceKind",
+    "EvidenceLedger",
+    "EvidenceSource",
     "InterviewQuestion",
     "InterviewQuestionType",
     "InterviewSession",
@@ -21,6 +26,7 @@ __all__ = [
     "LearningResource",
     "PracticeTask",
     "ReadinessCheck",
+    "SupportStatus",
     "canonical_job_key",
     "canonicalize_url",
 ]
