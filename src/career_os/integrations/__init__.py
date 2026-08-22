08461e1261605e719e9468ba30e57db9568fab90
Subratrout-486 <subratrout486@gmail.com>
2026-08-22 00:41:36 +0000

    feat: expose ATS integration layer

---
+++ src/career_os/integrations/__init__.py
@@ -0,0 +1,3 @@
+from career_os.integrations.ats import ATSClient, AshbyAdapter, GreenhouseAdapter, LeverAdapter, RawATSJob, detect_ats
+
+__all__ = ["ATSClient", "AshbyAdapter", "GreenhouseAdapter", "LeverAdapter", "RawATSJob", "detect_ats"]
