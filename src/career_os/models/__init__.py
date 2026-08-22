6cf128a8d8991e01f7d5681f2e8c3052711c5cb4
Subratrout-486 <subratrout486@gmail.com>
2026-08-22 13:46:37 +0000

    feat: export learning models

--- src/career_os/models/__init__.py
+++ src/career_os/models/__init__.py
@@ -1,6 +1,7 @@
 from career_os.models.application import ApplicationEvent, ApplicationRecord, ApplicationStatus
 from career_os.models.interview import AnswerEvaluation, AnswerScore, InterviewQuestion, InterviewQuestionType, InterviewSession
 from career_os.models.job import JobEvidence, JobRecord, JobStatus, canonical_job_key, canonicalize_url
+from career_os.models.learning import LearningObjective, LearningPlan, LearningPriority, LearningResource, PracticeTask, ReadinessCheck
 
 __all__ = [
     "AnswerEvaluation",
@@ -14,6 +15,12 @@ __all__ = [
     "JobEvidence",
     "JobRecord",
     "JobStatus",
+    "LearningObjective",
+    "LearningPlan",
+    "LearningPriority",
+    "LearningResource",
+    "PracticeTask",
+    "ReadinessCheck",
     "canonical_job_key",
     "canonicalize_url",
 ]
