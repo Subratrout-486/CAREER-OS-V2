709aef5c43b0c11bafb17e8f825ce5cfb9ab2e6a
Subratrout-486 <subratrout486@gmail.com>
2026-08-23 12:30:00 +0000

    feat: add application browser workflow package

---
+++ src/career_os/application/__init__.py
@@ -0,0 +1,11 @@
+"""Application workflow helpers."""
+
+from .browser_runner import ApplicationForm, BrowserApplicationRunner, FieldMapping, FormField, map_profile_fields
+
+__all__ = [
+    "ApplicationForm",
+    "BrowserApplicationRunner",
+    "FieldMapping",
+    "FormField",
+    "map_profile_fields",
+]
